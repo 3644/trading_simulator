d2bd2b18975011a0a2c33940a4eae33a4ff973c0
3644 <laltdehosata@gmail.com>
2025-03-11 02:52:53 +0100

    Fix Jsp

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+try {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} catch (error) {
+  document.body.innerHTML = `<div style="color:red; padding:20px;">
+    <h1>Erreur durant le chargement:</h1>
+    <pre>${error.message}</pre>
+  </div>`;
+  console.error(error);
+}
